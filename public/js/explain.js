121ffcc4b3d4b66d01389ea65fea605096f1387b
Hubert depesz Lubaczewski <depesz@depesz.com>
2011-06-14 14:19:51 +0200

    Hints for adding explains work.
    Again, thanks to metys for help - wouldn't be able to do it on my own.

--- public/js/explain.js
+++ public/js/explain.js
@@ -314,5 +314,24 @@
         }
 
   };
+    $( document ).ready( function() {
+        $('INPUT.auto-hint, TEXTAREA.auto-hint').focus(function(){
+            if($(this).val() == $(this).attr('title')){
+                $(this).val('');
+                $(this).removeClass('auto-hint');
+            }
+        });
+        $('INPUT.auto-hint, TEXTAREA.auto-hint').blur(function(){
+            if($(this).val() == '' && $(this).attr('title') != ''){
+                $(this).val($(this).attr('title'));
+                $(this).addClass('auto-hint');
+            }
+        });
+        $('INPUT.auto-hint, TEXTAREA.auto-hint').each(function(){
+            if($(this).attr('title') == ''){ return; }
+            if($(this).val() == ''){ $(this).val($(this).attr('title')); }
+            else { $(this).removeClass('auto-hint'); }
+        });
+    });
 
 } )( jQuery );
